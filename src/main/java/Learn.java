964628a9dbd9b3b1305c4e17f9ae8316a42a8ab9
wanghe <2053199927@qq.com>
2023-09-17 00:03:05 +0800

    add string huwenbin

--- src/main/java/Learn.java
+++ src/main/java/Learn.java
@@ -2,6 +2,7 @@ public class Learn {
     public static void main(String[] args) {
         int i=1;
         String s = "wanghe";
+        String sb = "huwenbin";
         System.out.println("wanhe shi zhu");
     }
 }

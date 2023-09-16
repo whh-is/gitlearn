858e87ecb4229393486eea98c2f9d6d101fb9f77
wanghe <2053199927@qq.com>
2023-09-17 00:32:28 +0800

    wanghe add bbb

--- src/main/java/Tra.java
+++ src/main/java/Tra.java
@@ -2,5 +2,6 @@ public class Tra {
     public static void main(String[] args) {
         System.out.println("wanghe shi meinv");
         System.out.println("aaa");
+        System.out.println("bbb");
     }
 }
